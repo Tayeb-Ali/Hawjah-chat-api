3834dca05eaa2e1ef7f557e35b0edff1fdedeb7c
Tayeb-Ali <Mr.elteb@gmail.com>
2020-05-24 21:30:28 +0100

    yesss  fix save messag in db with typeOrm

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ let socketioJwt = require('socketio-jwt');
 var myEnv = require('dotenv').config({ path: '.env' });
 const typeorm = require("typeorm"); // import * as typeorm from "typeorm";
 const UserChat = require("./models/UserChat").UserChat
+const UserChatEntity = require("./entity/UserChatSchema");
 
 
 
 let userObj = { id: null, name: null, }
-    // const db =
+const db = typeorm.createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "medical",
+    synchronize: true,
+    logging: false,
+    entities: [
+        UserChatEntity,
+    ]
+});
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
@@ -28,50 +41,35 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', function() {
         io.emit('users-changed', { user: socket.username, event: 'left' });
-    });
+    }); //end disconnect user funcation 
 
     socket.on('set-user', (user) => {
         socket.username = user.name;
         userObj = user;
 
         io.emit('users-changed', { user: user.name, event: 'joined' });
-    });
+    }); // end socket response from user and save data in userObject.
 
     // start mysql conn
     socket.on('send-message', (message) => {
         let createdAt = new Date("2020-05-19 12:48:56")
         io.emit('message', { msg: message.text, user: socket.username, createdAt: createdAt });
-        typeorm.createConnection({
-            type: "mysql",
-            host: "localhost",
-            port: 3306,
-            username: "root",
-            password: "",
-            database: "medical",
-            synchronize: true,
-            logging: false,
-            entities: [
-                require("./entity/UserChatSchema"),
-            ]
-        }).then(function(connection) {
+        db.then(function(connection) {
             let userChat = new UserChat();
             userChat.message = message.text;
             userChat.from_user_id = userObj.id;
             userChat.to_user_id = 2;
-            return connection
-                .manager
-                .save([userChat])
-                // let userChatRepository = connection.getRepository(UserChat);
-                // userChatRepository.save(userChat)
-                .then(data => {
-                    console.log("message has been saved: ", data);
-                    //         console.log("Now lets load all message: ");
+            const userChatRepository = connection.getRepository("user_chat");
+            userChatRepository.save(userChat)
+                .then(function(savedChat) {
+                    console.log("message has been saved: ", savedChat);
+                    console.log("Now lets load all message: ");
 
-                    // return userChatRepository.find();
+                    return userChatRepository.find();
                 })
-                // .catch(error => {
-                //     console.log('error: ', error)
-                // })
+                .catch(error => {
+                    console.log('error: ', error)
+                })
         }); //end of conn fun
 
     }); // socket response
